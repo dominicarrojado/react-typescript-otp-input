09b46004bf222af64c338b3c85c7b11eceed0171
Dominic Arrojado <dominicarrojado@gmail.com>
2022-06-14 15:48:47 +0800

    construct array from value prop string for input boxes

--- src/components/OtpInput.tsx
+++ src/components/OtpInput.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+import { RE_DIGIT } from '../constants';
 import './OtpInput.css';
 
 type Props = {
@@ -6,19 +8,36 @@ type Props = {
   onChange: (value: string) => void;
 };
 
-export default function OtpInput(props: Props) {
+export default function OtpInput({ value, valueLength, onChange }: Props) {
+  const valueItems = useMemo(() => {
+    const valueArray = value.split('');
+    const items: Array<string> = [];
+
+    for (let i = 0; i < valueLength; i++) {
+      const char = valueArray[i];
+
+      if (RE_DIGIT.test(char)) {
+        items.push(char);
+      } else {
+        items.push('');
+      }
+    }
+
+    return items;
+  }, [value, valueLength]);
+
   return (
     <div className="otp-group">
-      {[1, 2, 3, 4, 5, 6].map((value, idx) => (
+      {valueItems.map((digit, idx) => (
         <input
           key={idx}
           type="text"
           inputMode="numeric"
           autoComplete="one-time-code"
           pattern="\d{1}"
-          maxLength={1}
+          maxLength={valueLength}
           className="otp-input"
-          value={value}
+          value={digit}
         />
       ))}
     </div>
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const RE_DIGIT = new RegExp(/^\d+$/);
