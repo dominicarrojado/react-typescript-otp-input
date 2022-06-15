8caacebfa8d071713a8166c3b76263e2d9bbcaab
Dominic Arrojado <dominicarrojado@gmail.com>
2022-06-15 18:32:00 +0800

    write tests to allow typing of digits

--- src/components/__tests__/OtpInput.test.tsx
+++ src/components/__tests__/OtpInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { faker } from '@faker-js/faker';
 import OtpInput, { Props } from '../OtpInput';
 
@@ -22,6 +22,38 @@ describe('<OtpInput />', () => {
 
     inputEls.forEach((inputEl, idx) => {
       expect(inputEl).toHaveValue(valueArray[idx]);
+    });
+  });
+
+  it('should allow typing of digits', () => {
+    const valueLength = faker.datatype.number({ min: 2, max: 6 });
+    const onChange = jest.fn();
+
+    renderComponent({
+      valueLength,
+      onChange,
+      value: '',
+    });
+
+    const inputEls = screen.queryAllByRole('textbox');
+
+    expect(inputEls).toHaveLength(valueLength);
+
+    inputEls.forEach((inputEl, idx) => {
+      const digit = faker.datatype.number({ min: 0, max: 9 }).toString();
+
+      fireEvent.change(inputEl, {
+        target: { value: digit },
+      });
+
+      expect(onChange).toBeCalledTimes(1);
+      expect(onChange).toBeCalledWith(digit);
+
+      const inputFocused = inputEls[idx + 1] || inputEl;
+
+      expect(inputFocused).toHaveFocus();
+
+      onChange.mockReset();
     });
   });
 });
